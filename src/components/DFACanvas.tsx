e24b3e4c2c9293f4a99a8b3d79f253ba9908f2ee
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-02-02 05:25:06 +0000

    grouping inside svg

--- src/components/DFACanvas.tsx
+++ src/components/DFACanvas.tsx
@@ -1,8 +1,20 @@
 function DFACanvas() {
     return (
-        <svg width="400" height="200" style={{ border: "1px solid black" }}>
-            <circle cx="200" cy="100" r="40" fill="lightblue" />
-        </svg>
+    <svg width="400" height="200" style={{ border: "1px solid black" }}>
+      <g>
+        <circle cx="120" cy="100" r="30" fill="lightblue" />
+        <text x="120" y="105" textAnchor="middle">
+          q0
+        </text>
+      </g>
+
+      <g>
+        <circle cx="280" cy="100" r="30" fill="lightgreen" />
+        <text x="280" y="105" textAnchor="middle">
+          q1
+        </text>
+      </g>
+    </svg>
     );
   }
   
