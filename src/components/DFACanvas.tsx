5860dfcd8032b6f9837b0e85b8f0dc6033f27728
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-02-02 05:36:43 +0000

    line between two state circles

--- src/components/DFACanvas.tsx
+++ src/components/DFACanvas.tsx
@@ -15,6 +15,15 @@ function DFACanvas() {
 
     return (
         <svg width="400" height="200" style={{ border: "1px solid black" }}>
+
+            <line
+                x1={states[0].x}
+                y1={states[0].y}
+                x2={states[1].x}
+                y2={states[1].y}
+                stroke="black"
+             />
+
             {states.map((state) => (
                 <g key={state.id}>
                     <circle cx={state.x} cy={state.y} r={30} stroke="black" strokeWidth="2" fill={state.color} />
