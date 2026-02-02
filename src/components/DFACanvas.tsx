79c999e93e25a6562551afe5cc929f456b254db7
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-02-02 05:33:13 +0000

    introduce states based rendering and ts types

--- src/components/DFACanvas.tsx
+++ src/components/DFACanvas.tsx
@@ -1,21 +1,30 @@
 function DFACanvas() {
-    return (
-    <svg width="400" height="200" style={{ border: "1px solid black" }}>
-      <g>
-        <circle cx="120" cy="100" r="30" fill="lightblue" />
-        <text x="120" y="105" textAnchor="middle">
-          q0
-        </text>
-      </g>
 
-      <g>
-        <circle cx="280" cy="100" r="30" fill="lightgreen" />
-        <text x="280" y="105" textAnchor="middle">
-          q1
-        </text>
-      </g>
-    </svg>
+    type state = {
+        id: string;
+        x: number;
+        y: number;
+        color: string;
+    }[]
+
+    const states:state = [
+        { id: "q0", x: 120, y: 100, color: "lightyellow" },
+        { id: "q1", x: 280, y: 100, color: "lightgreen" },
+      ];
+
+
+    return (
+        <svg width="400" height="200" style={{ border: "1px solid black" }}>
+            {states.map((state) => (
+                <g key={state.id}>
+                    <circle cx={state.x} cy={state.y} r={30} stroke="black" strokeWidth="2" fill={state.color} />
+                    <text x={state.x} y={state.y} textAnchor="middle" dominantBaseline="middle" fontSize="16">
+                        {state.id}
+                    </text>
+                </g>
+            ))}
+        </svg>
     );
-  }
+}
   
-  export default DFACanvas;
+export default DFACanvas;
