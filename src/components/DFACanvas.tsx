5bfe653462e94c3ff0b3ffa10de11fbb5c09859a
Sran012 <88543485+Sran012@users.noreply.github.com>
2026-02-02 04:48:05 +0000

    svg canvas component

---
+++ src/components/DFACanvas.tsx
@@ -0,0 +1,9 @@
+function DFACanvas() {
+    return (
+        <svg width="400" height="200" style={{ border: "1px solid black" }}>
+            <circle cx="200" cy="100" r="40" fill="lightblue" />
+        </svg>
+    );
+  }
+  
+  export default DFACanvas;
